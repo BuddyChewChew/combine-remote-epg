77c3b2daefa2c640f362307dfd122888307db682
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-03-10 11:40:50 -0400

    Update combine_epgs.py

--- combine_epgs.py
+++ combine_epgs.py
@@ -7,8 +7,7 @@ epg_urls = [
     "https://bit.ly/moj-epg",
     "https://raw.githubusercontent.com/ydbf/MoveOnJoy/refs/heads/main/epg.xml",
     "https://raw.githubusercontent.com/acidjesuz/EPGTalk/master/guide.xml",
-    "https://epgshare01.online/epgshare01/epg_ripper_ALL_SOURCES1.xml.gz",
-    "https://raw.githubusercontent.com/BuddyChewChew/tubi-scraper/refs/heads/main/tubi_epg.xml"
+    "https://epgshare01.online/epgshare01/epg_ripper_ALL_SOURCES1.xml.gz"
 ]
 
 # Output file
