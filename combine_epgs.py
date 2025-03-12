a7b6420d50f6b96f13e3cc52b8621853512a0f72
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-03-12 13:12:25 -0400

    Update combine_epgs.py

--- combine_epgs.py
+++ combine_epgs.py
@@ -4,7 +4,6 @@ from xml.etree import ElementTree as ET
 # List of EPG URLs to merge
 epg_urls = [
     "https://tvpass.org/epg.xml",
-    "https://bit.ly/moj-epg",
     "https://raw.githubusercontent.com/ydbf/MoveOnJoy/refs/heads/main/epg.xml",
     "https://raw.githubusercontent.com/acidjesuz/EPGTalk/master/guide.xml",
     "https://epgshare01.online/epgshare01/epg_ripper_ALL_SOURCES1.xml.gz"
