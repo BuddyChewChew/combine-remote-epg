ae873bfc6f60fddefb8b397afb8258ebd2702599
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-03-12 20:19:02 -0400

    Update combine_epgs.py

--- combine_epgs.py
+++ combine_epgs.py
@@ -3,10 +3,14 @@ from xml.etree import ElementTree as ET
 
 # List of EPG URLs to merge
 epg_urls = [
-    "https://tvpass.org/epg.xml",
-    "https://raw.githubusercontent.com/ydbf/MoveOnJoy/refs/heads/main/epg.xml",
+    "https://github.com/pigzillaaaaa/iptv-scraper/raw/refs/heads/main/epgs/tvpass-epg.xml.gz",
+    "https://github.com/pigzillaaaaa/iptv-scraper/raw/refs/heads/main/epgs/moveonjoy-epg.xml.gz",
+    "https://github.com/pigzillaaaaa/iptv-scraper/raw/refs/heads/main/epgs/daddylive-epg.xml.gz",
     "https://raw.githubusercontent.com/acidjesuz/EPGTalk/master/guide.xml",
-    "https://epgshare01.online/epgshare01/epg_ripper_ALL_SOURCES1.xml.gz"
+    "https://epgshare01.online/epgshare01/epg_ripper_ALL_SOURCES1.xml.gz",
+    "https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz",
+    "https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz",
+    "https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz"
 ]
 
 # Output file
